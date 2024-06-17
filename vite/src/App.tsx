114dd90f2172eb07eb25bbaa9c8135de2446d5a2
gyuhaaa <vkvkvk2892@gmail.com>
2024-06-17 09:45:49 +0900

    Chakra UI & ethers Setting

---
+++ vite/src/App.tsx
@@ -0,0 +1,55 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { Contract, ethers } from "ethers";
+import { JsonRpcSigner } from "ethers";
+import { FC, useEffect, useState } from "react";
+import mintNftAbi from "./mintNftAbi.json";
+
+const App: FC = () => {
+  const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const [contract, setContract] = useState<Contract | null>(null);
+
+  const onClickMetamask = async () => {
+    try {
+      if (!window.ethereum) return;
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      setSigner(await provider.getSigner());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!signer) return;
+
+    setContract(
+      new Contract(
+        "0x82E6092930f649eFA45dE7b5b1A71d07c151bD4B",
+        mintNftAbi,
+        signer
+      )
+    );
+  }, [signer]);
+
+  useEffect(() => console.log(contract), [contract]);
+
+  return (
+    <Flex
+      bgColor="red.100"
+      w="100%"
+      minH="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+    >
+      {signer ? (
+        <Text>{signer.address}</Text>
+      ) : (
+        <Button onClick={onClickMetamask}>🦊 로그인</Button>
+      )}
+    </Flex>
+  );
+};
+
+export default App;
