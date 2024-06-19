801af67def4cceb0484dbb594f604bfc23dbd6b9
gyuhaaa <vkvkvk2892@gmail.com>
2024-06-19 10:19:09 +0900

    modify pinata address

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -66,7 +66,7 @@ const App: FC = () => {
         }
       );
 
-      return `https://slime-project.mypinata.cloud/ipfs/${response.data.IpfsHash}`;
+      return `https://jade-junior-ape-105.mypinata.cloud/ipfs/${response.data.IpfsHash}`;
     } catch (error) {
       console.error(error);
     }
